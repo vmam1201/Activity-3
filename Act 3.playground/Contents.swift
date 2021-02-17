5cfc613f7480abf42aed565c335a747db24502d3
vmam1201 <79224821+vmam1201@users.noreply.github.com>
2021-02-17 15:22:42 -0800

    Add files via upload

---
+++ Act 3.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+//Parte 1
+var numero = 2
+let otro_numero:Int = 7
+var texto = "Actividad 3"
+let otro_texto:String = "Actividad 3, Parte 2"
+var decimal = 1.12
+let otro_decimal:Float = 3.12
+var cierto = true
+let falso:Bool = false
+
+//Array
+var numeros:Array<Int> = Array<Int>()
+numeros.append(1)
+numeros.append(2)
+numeros.append(3)
+numeros.append(4)
+numeros.append(5)
+numeros.append(6)
+numeros.append(7)
+numeros.append(8)
+numeros.append(9)
+numeros.append(10)
+numeros.count
+
+//Diccionario
+var diaSemana:Dictionary<String, Int> = Dictionary <String, Int>()
+diaSemana["Lunes"] = 1
+diaSemana["Martes"] = 2
+diaSemana["Miercoles"] = 3
+diaSemana["Jueves"] = 4
+diaSemana["Viernes"] = 5
+diaSemana["Sabado"] = 6
+diaSemana["Domingo"] = 7
+print("Dias de la semana: \(diaSemana)")
+	
